c77c50396636ac7521ebfb23fd1d56b3eefef35c
Polylanger <geekanddentist@gmail.com>
2017-04-09 15:37:06 +0800

    Salary Payment System: <ServiceCharge>

--- src/main/java/salary_payment/Affiliation.java
+++ src/main/java/salary_payment/Affiliation.java
@@ -1,8 +1,14 @@
 package salary_payment;
 
+import java.util.Date;
+
 /**
  * Created by Polylanger on 2017/4/9.
  */
-public class Affiliation {
+public abstract class Affiliation {
+
+    public abstract ServiceCharge getServiceCharge(Date date);
+
+    public abstract void addServiceCharge(ServiceCharge serviceCharge);
 
 }
---
+++ src/main/java/salary_payment/NoAffiliation.java
@@ -0,0 +1,17 @@
+package salary_payment;
+
+import java.util.Date;
+
+/**
+ * Created by Polylanger on 2017/4/9.
+ */
+public class NoAffiliation extends Affiliation {
+
+    public ServiceCharge getServiceCharge(Date date) {
+        return null;
+    }
+
+    public void addServiceCharge(ServiceCharge serviceCharge) {
+
+    }
+}
